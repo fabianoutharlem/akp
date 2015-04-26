974f158e265d875acfc3f9a54c1164e176dd75d3
Fabian Oudhaarlem <fabian@coqtail.com>
2015-04-26 16:12:51 +0200

    Made the class for importing, no content yet

---
+++ app/controllers/importer/import_controller.rb
@@ -0,0 +1,24 @@
+module Import
+  class ImportController
+
+    def create
+
+    end
+
+    def update
+
+    end
+
+    def destroy
+
+    end
+
+    private
+
+    def car_data
+      body = request.body.read
+
+    end
+
+  end
+end
