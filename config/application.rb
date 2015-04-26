c0f96fdfd8307d9d88a1e5c4be608ad69e1e0588
Fabian Oudhaarlem <fabian@coqtail.com>
2015-04-26 16:14:39 +0200

    added less compilation for admin theme

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,12 @@ Bundler.require(*Rails.groups)
 
 module Akp
   class Application < Rails::Application
+
+    config.assets.paths << File.join(Rails.root, "/vendor/pages")
+
+    config.less.paths << "#{Rails.root}/app/assets/admin/stylesheets"
+    config.less.compress = true
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
