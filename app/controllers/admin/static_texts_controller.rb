2646a554ed1429e0c911fefb00bee54d591060b7
Fabian Oudhaarlem <fabian@coqtail.com>
2015-05-11 21:55:45 +0200

    Finished static text admin

--- app/controllers/admin/static_texts_controller.rb
+++ app/controllers/admin/static_texts_controller.rb
@@ -10,10 +10,19 @@ module Admin
       begin
         static_texts = StaticText.find(params[:static_texts].keys)
         static_texts.each do |static_text|
-          # static_text.value = params[:static_texts][static_text.id][:value]
-          # static_text.save!
-          puts params[:static_texts][static_text.id]
+          static_text.value = params[:static_texts][static_text.id.to_s]['value']
+          static_text.save!
+          unless static_text.valid?
+            flash[:error] = [] if flash[:error].nil?
+            flash[:error] << 'The text with key ' + static_text.key + 'was not updated'
+          end
         end
+      rescue
+        flash[:error] = ['Something went wrong, nothing was updated, please try again.']
+      end
+
+      if flash[:error].blank?
+        flash[:notice] = ['The static texts were all updated succesfully']
       end
 
       redirect_to admin_static_texts_path
