482e02b624f3e03a67757c377679194eb9c4d20e
Fabian Oudhaarlem <fabian@coqtail.com>
2015-06-07 18:55:32 +0200

    breadcrumbs for blog pages and admin menu fix

--- app/controllers/blog_pages_controller.rb
+++ app/controllers/blog_pages_controller.rb
@@ -2,6 +2,7 @@ class BlogPagesController < ApplicationController
 
   def show
     @blog_page = BlogPage.find(params[:id])
+    add_breadcrumb @blog_page.title
     if @blog_page.templateable_type
       render @blog_page.templateable_type.to_s.underscore
     end
