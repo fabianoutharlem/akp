296ccdd1f2aca407cfabe46a8c364b6080d66995
Fabian Oudhaarlem <fabian@coqtail.com>
2015-06-08 22:44:28 +0200

    fix for fonts

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,8 @@ module Akp
   class Application < Rails::Application
 
     config.assets.paths << File.join(Rails.root, "/vendor/pages")
+    config.assets.paths << Rails.root.join('app', 'assets', 'fonts')
+    config.assets.precompile += %w( .svg .eot .woff .ttf )
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
