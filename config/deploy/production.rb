4ca7868a6f593c1ff0878cd4c4a2db5f22541437
Fabian Oudhaarlem <fabian@coqtail.com>
2015-07-22 21:08:23 +0200

    work on production capfile

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -5,7 +5,7 @@ set :deploy_to, '/data/akp_production'
 
 server '188.166.92.89', user: fetch(:user), password: fetch(:password), roles: %w{web app db}
 
-set :stage, :staging
+set :stage, :production
 
 set :branch, 'master'
 set :env, 'production'
