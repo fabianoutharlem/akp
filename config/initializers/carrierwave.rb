d7629b82cf2d3030acc948ebb6a226577a25af4f
Fabian Oudhaarlem <fabian@coqtail.com>
2015-11-10 23:47:23 +0100

    fix

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,15 +1,4 @@
 CarrierWave.configure do |config|
-  config.fog_provider = 'fog/aws'                        # required
-  config.fog_credentials = {
-      provider:              'AWS',                        # required
-      aws_access_key_id:     'AKIAJYRWYDU3OSUJ3O6Q',                        # required
-      aws_secret_access_key: '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',                        # required
-      region:                'eu-central-1'                  # optional, defaults to 'us-east-1'
-  }
-  config.fog_directory  = 'akp-' + Rails.env                          # required
-
-
-
 
   config.storage    = :aws
   config.aws_bucket = 'akp-' + Rails.env
