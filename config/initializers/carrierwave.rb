fb0369ee1984c4816cc511df8e1cea61879a3434
Fabian Oudhaarlem <fabian@coqtail.com>
2015-11-10 23:16:15 +0100

    fix

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -5,9 +5,6 @@ CarrierWave.configure do |config|
       aws_access_key_id:     'AKIAJYRWYDU3OSUJ3O6Q',                        # required
       aws_secret_access_key: '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',                        # required
       region:                'eu-central-1',                  # optional, defaults to 'us-east-1'
-      host:                  'https://akp-' + Rails.env + '.s3.amazonaws.com'
+      endpoint:                  'https://akp-' + Rails.env + '.s3.amazonaws.com'
   }
-  config.fog_directory  = 'akp-' + Rails.env                          # required
-  config.fog_public     = true                                        # optional, defaults to true
-  config.fog_attributes = { 'Cache-Control' => "max-age=#{365.day.to_i}" } # optional, defaults to {}
 end
