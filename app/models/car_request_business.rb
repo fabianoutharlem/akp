f62222a4ba7174197666d7cf40edab07f0395e5a
Fabian Oudhaarlem <fabian@coqtail.com>
2015-11-18 22:06:08 +0100

    partial path for bussiness request

--- app/models/car_request_business.rb
+++ app/models/car_request_business.rb
@@ -1,3 +1,14 @@
 class CarRequestBusiness < ActiveRecord::Base
   belongs_to :car
+
+  before_destroy :invalidate_cache
+
+  def invalidate_cache
+    expire_fragment("admin_car_request_#{self.id}")
+  end
+
+  def to_partial_path
+    'car_requests/car_request'
+  end
+
 end
