98535863b9b9441087446f520ab5493ce4487679
Fabian Oudhaarlem <fabian@coqtail.com>
2015-11-27 20:54:06 +0100

    search includes

--- app/models/car.rb
+++ app/models/car.rb
@@ -52,7 +52,7 @@ class Car < ActiveRecord::Base
     puts build_query(params).to_json
     search = Car.search(build_query(params).to_json)
     if search.results.total
-      search.records.to_a
+      search.records.includes(:brand, :model).to_a
     else
       nil
     end
