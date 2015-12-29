3eab4255b912d41039cd8054a66a167b0c6b34fb
Fabian Oudhaarlem <fabian@coqtail.com>
2015-12-29 22:58:41 +0100

    add breadcrumbs to action pages

--- app/controllers/action_pages_controller.rb
+++ app/controllers/action_pages_controller.rb
@@ -1,8 +1,11 @@
 class ActionPagesController < ApplicationController
 
+  add_breadcrumb 'Acties'
+
   def show
     @action_page = ActionPage.find(params[:id])
     @cars = @action_page.cars.includes(:brand, :model).order(order_hash).page(params[:page]).per(8) if @action_page.present?
+    add_breadcrumb @action_page.banner_title
   end
 
 end
