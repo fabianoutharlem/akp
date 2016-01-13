0fd24c7f4b244bcc58ad6460f4ce37ab5aaf5e6a
Fabian Oudhaarlem <fabian@coqtail.com>
2016-01-13 21:08:13 +0100

    fix

--- db/migrate/20160113195556_add_pro_fields_to_action_pages.rb
+++ db/migrate/20160113195556_add_pro_fields_to_action_pages.rb
@@ -7,16 +7,19 @@ class AddProFieldsToActionPages < ActiveRecord::Migration
     add_column :action_pages, :pro_5, :string
     add_column :action_pages, :pro_6, :string
     add_column :action_pages, :pro_7, :string
+
+    ActionPage.update_all(
+        pro_1: 'Groot aanbod',
+        pro_2: 'Onderhoudshistorie & schadeverleden bekend',
+        pro_3: 'Standaard 3 maanden garantie',
+        pro_4: 'Goed onderhouden',
+        pro_5: 'Gunstige aanschafprijs',
+        pro_6: 'Van de 1e eigenaar',
+        pro_7: 'Mogelijkheid om uw auto te financieren',
+    )
+
   end
 
-  ActionPage.update_all(
-                pro_1: 'Groot aanbod',
-                pro_2: 'Onderhoudshistorie & schadeverleden bekend',
-                pro_3: 'Standaard 3 maanden garantie',
-                pro_4: 'Goed onderhouden',
-                pro_5: 'Gunstige aanschafprijs',
-                pro_6: 'Van de 1e eigenaar',
-                pro_7: 'Mogelijkheid om uw auto te financieren',
-  )
+
 
 end
